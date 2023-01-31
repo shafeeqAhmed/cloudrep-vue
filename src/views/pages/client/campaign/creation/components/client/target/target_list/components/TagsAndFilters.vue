75ecb53619a26938f7c8954666450653af9b9a57
Zohaib Mazhar <110980291+zohaibmazhar075@users.noreply.github.com>
2023-01-31 15:56:43 +0500

    tags and filters

--- src/views/pages/client/campaign/creation/components/client/target/target_list/components/TagsAndFilters.vue
+++ src/views/pages/client/campaign/creation/components/client/target/target_list/components/TagsAndFilters.vue
@@ -2,136 +2,73 @@
   <div>
     <div class="">
       <div class="">
-        <b-button
-          :id="target_uuid"
-          class="popover-btn"
-          @click="openPopup"
-        >
-          <feather-icon
-            icon="FilterIcon"
-            class="cursor-pointer"
-            size="18"
-          />
+        <b-button :id="target_uuid" class="popover-btn" @click="openPopup">
+          <feather-icon icon="FilterIcon" class="cursor-pointer" size="18" />
         </b-button>
       </div>
 
-      <b-popover
-        :target="target_uuid"
-        custom-class="route_filter_modal"
-        :show.sync="show"
-        placement="left"
-      >
+      <b-popover :target="target_uuid" custom-class="route_filter_modal" :show.sync="show" placement="left">
         <div class="route_condition">
-          <h4
-            class="mt-2 mb-1 d-flex align-items-center justify-content-between"
-          >
+          <h4 class="mt-1 mb-1 d-flex align-items-center justify-content-between">
             Filter Conditions
-            <feather-icon
-              icon="XIcon"
-              class="cursor-pointer"
-              size="21"
-              @click="onClose"
-            />
+            <feather-icon icon="XIcon" class="cursor-pointer" size="21" @click="onClose" />
           </h4>
           <!--============================================================================-->
           <!--============================================================================-->
-          <div
-            class=""
-            v-for="(condition, key) in conditions"
-            :key="key"
-          >
+          <div class="" v-for="(condition, key) in conditions" :key="key">
             <!--==============================-->
-            <div
-              class="d-flex align-items-center justify-content-center mt-1 mb-1"
-              v-if="key != 0"
-            >
+            <div class="d-flex align-items-center justify-content-center mt-1 mb-1" v-if="key != 0">
               <hr />
               <p class="m-0 mr-1 ml-1">{{ condition.operation }}</p>
               <hr />
             </div>
             <!--==============================-->
             <div class="route_condition_card">
-              <div
-                class="d-flex align-items-center justify-content-end mb-1"
-                v-if="key != 0"
-              >
+              <div class="d-flex align-items-center justify-content-end mb-1" v-if="key != 0">
                 <div class="">
-                  <feather-icon
-                    icon="XIcon"
-                    class="cursor-pointer"
-                    @click="removeFilterCondition(key)"
-                    size="21"
-                  />
+                  <feather-icon icon="XIcon" class="cursor-pointer" @click="removeFilterCondition(key)" size="21" />
                 </div>
               </div>
               <div class="">
                 <div class="input-gradient mb-1">
-                  <v-select
-                    :options="tags"
-                    v-model="condition.tag"
-                    label="name"
-                    :reduce="(list) => list.uuid"
-                    @input="getTagFilters($event, key)"
-                    placeholder="Select Tag"
-                  ></v-select>
+                  <v-select :options="tags" v-model="condition.tag" label="name" :reduce="(list) => list.uuid"
+                    @input="getTagFilters($event, key)" placeholder="Select Tag"></v-select>
                 </div>
                 <div class="input-gradient mb-1">
-                  <v-select
-                    :options="condition.operators"
-                    v-model="condition.operator"
-                    :reduce="(list) => list.uuid"
-                    label="name"
-                    placeholder="Operators"
-                  ></v-select>
+                  <v-select :options="condition.operators" v-model="condition.operator" :reduce="(list) => list.uuid"
+                    label="name" placeholder="Operators"></v-select>
                 </div>
                 <div class="router_states">
-                  <div class="left_state_box">
-                    <b-list-group>
-                      <b-list-group-item
-                        v-for="(state, stateIndex) in condition.states"
-                        :ref="`state-${stateIndex}${key}`"
-                        :key="`state-${stateIndex}${key}`"
-                        @click="
-                          selectOperatorValue(stateIndex, key, state.name)
-                        "
-                        >{{ state.name }}</b-list-group-item
-                      >
-                    </b-list-group>
+                  <div class="input-gradient">
+                    <div class="left_state_box">
+                      <b-list-group>
+                        <b-list-group-item v-for="(state, stateIndex) in condition.states"
+                          :ref="`state-${stateIndex}${key}`" :key="`state-${stateIndex}${key}`" @click="
+                            selectOperatorValue(stateIndex, key, state.name)
+                          ">{{ state.name }}</b-list-group-item>
+                      </b-list-group>
+                    </div>
                   </div>
                   <div class="center_state_box">
                     <div class="mb-1">
-                      <feather-icon
-                        icon="ArrowRightIcon"
-                        class="cursor-pointer"
-                        size="21"
-                        @click="assignAllOperatorValue(key)"
-                      />
+                      <feather-icon icon="ArrowRightIcon" class="cursor-pointer" size="21"
+                        @click="assignAllOperatorValue(key)" />
                     </div>
                     <div class="mb-1">
-                      <feather-icon
-                        icon="RepeatIcon"
-                        class="cursor-pointer"
-                        size="21"
-                      />
+                      <feather-icon icon="RepeatIcon" class="cursor-pointer" size="21" />
                     </div>
                     <div class="mb-1">
-                      <feather-icon
-                        icon="ArrowLeftIcon"
-                        class="cursor-pointer"
-                        size="21"
-                        @click="removeAllOperatorValue(key)"
-                      />
+                      <feather-icon icon="ArrowLeftIcon" class="cursor-pointer" size="21"
+                        @click="removeAllOperatorValue(key)" />
                     </div>
                   </div>
-                  <div class="right_state_box">
-                    <b-list-group>
-                      <b-list-group-item
-                        v-for="(state, valueIndex) in condition.val"
-                        :key="valueIndex"
-                        @click="removeOperatorValue(valueIndex, key, state)"
-                        >{{ state }}</b-list-group-item
-                      >
-                    </b-list-group>
+                  <div class="input-gradient">
+                    <div class="right_state_box">
+                      <b-list-group>
+                        <b-list-group-item v-for="(state, valueIndex) in condition.val" :key="valueIndex"
+                          @click="removeOperatorValue(valueIndex, key, state)">{{ state }}</b-list-group-item>
+                      </b-list-group>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -139,31 +76,11 @@
           </div>
 
           <div class="router_rule mt-2 mb-1">
-            <b-button
-              variant="primary"
-              class="mr-1"
-              @click="addCondition('or')"
-              >ADD OR RULE</b-button
-            >
-            <b-button
-              variant="primary"
-              class="mr-1"
-              @click="addCondition('and')"
-              >ADD AND RULE</b-button
-            >
-            <b-button
-              variant="primary"
-              class="mr-1"
-              @click="onClose"
-              >skip</b-button
-            >
+            <b-button variant="primary" class="mr-1" @click="addCondition('or')">ADD OR RULE</b-button>
+            <b-button variant="primary" class="mr-1" @click="addCondition('and')">ADD AND RULE</b-button>
+            <b-button variant="primary" class="mr-1" @click="onClose">skip</b-button>
 
-            <b-button
-              variant="primary"
-              @click="saveFilterRecord"
-              class="mr-1"
-              >Apply Filter</b-button
-            >
+            <b-button variant="primary" @click="saveFilterRecord" class="mr-1">Apply Filter</b-button>
           </div>
         </div>
       </b-popover>
@@ -368,9 +285,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.route_filter_modal {
-  left: -150px !important;
-}
-</style>
